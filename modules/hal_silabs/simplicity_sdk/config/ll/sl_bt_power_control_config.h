9c529ac115211e3c8278efb094d62a4600712d16
Jori Rintahaka <jori.rintahaka@silabs.com>
2025-02-18 19:34:28 +0200

    wip: adding the last missing configurations

---
+++ modules/hal_silabs/simplicity_sdk/config/ll/sl_bt_power_control_config.h
@@ -0,0 +1,56 @@
+/***************************************************************************//**
+ * @file
+ * @brief Bluetooth LE Power Control configuration
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2025 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software in a
+ *    product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ ******************************************************************************/
+
+#ifndef SL_BT_POWER_CONTROL_CONFIG_H
+#define SL_BT_POWER_CONTROL_CONFIG_H
+
+#define SL_BT_ACTIVATE_POWER_CONTROL	CONFIG_BT_SILABS_EFR32_ACTIVATE_POWER_CONTROL
+
+#define SL_BT_GOLDEN_RSSI_MIN_1M	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MIN_1M
+
+#define SL_BT_GOLDEN_RSSI_MAX_1M	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MAX_1M
+
+#define SL_BT_GOLDEN_RSSI_MIN_2M	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MIN_2M
+
+#define SL_BT_GOLDEN_RSSI_MAX_2M	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MAX_2M
+
+#define SL_BT_GOLDEN_RSSI_MIN_CODED_S8	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MIN_CODED_S8
+
+#define SL_BT_GOLDEN_RSSI_MAX_CODED_S8	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MAX_CODED_S8
+
+#define SL_BT_GOLDEN_RSSI_MIN_CODED_S2	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MIN_CODED_S2
+
+#define SL_BT_GOLDEN_RSSI_MAX_CODED_S2	CONFIG_BT_SILABS_EFR32_GOLDEN_RSSI_MAX_CODED_S2
+
+#define SL_BT_DEFAULT_MAX_POWER_LEVEL	CONFIG_BT_SILABS_EFR32_DEFAULT_MAX_POWER_LEVEL
+
+#define SL_BT_DEFAULT_MIN_POWER_LEVEL	CONFIG_BT_SILABS_EFR32_DEFAULT_MIN_POWER_LEVEL
+
+#endif
